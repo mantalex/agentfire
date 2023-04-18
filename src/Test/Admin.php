cf870a9e57ef63a7ac3d93e7028907eb13f03573
Alex <ulkoalex@gmail.com>
2023-04-18 16:57:39 +0200

    acf options page backward compatibility

--- src/Test/Admin.php
+++ src/Test/Admin.php
@@ -15,6 +15,7 @@ class Admin {
 	use Singleton;
 
 	public $slug = 'test-settings';
+	public $key = 'test_settings';
 
 	public function __construct() {
 		add_action( 'admin_menu', [ $this, 'menu' ] );
@@ -27,7 +28,92 @@ class Admin {
 	public function menu() {
 		if ( function_exists( 'acf_add_options_page' ) ) {
 			// acf_add_options_page( [ ... ] );
+		} else {
+			$slug = add_menu_page( 'AgentFire Test', 'AgentFire Test', 'manage_options', 'test-settings', [ $this, 'renderPage' ] );
+			add_action( "load-{$slug}", [ $this, 'adminLoad' ] );
 		}
+	}
+
+	public function renderPage() {
+		$fields = acf_get_fields( $this->key );
+		$field_group = acf_get_field_group( $this->key );
+		$options = [
+			'id'         => 'acf-group_' . $this->key,
+			'key'        => $field_group['key'],
+			'style'      => $field_group['style'],
+			'label'      => $field_group['label_placement'],
+			'visibility' => true,
+		];
+		?>
+		<div class="wrap acf-settings-wrap">
+			<h1>AgentFire Test</h1>
+			<form id="post" method="post" name="post">
+				<?php
+				acf_form_data( [
+					'post_id' => 'options',
+					'nonce'   => 'options',
+				] );
+				?>
+				<div id="poststuff">
+					<div id="post-body" class="metabox-holder columns-2">
+						<div id="postbox-container-1" class="postbox-container">
+							<div id="side-sortables" class="meta-box-sortables ui-sortable">
+								<div id="submitdiv" class="postbox ">
+									<button type="button" class="handlediv" aria-expanded="true"><span class="toggle-indicator" aria-hidden="true"></span></button>
+									<h2 class="hndle ui-sortable-handle"><span>Save Settings</span></h2>
+									<div class="inside">
+										<div id="major-publishing-actions">
+											<div id="publishing-action">
+												<span class="spinner"></span>
+												<input type="submit" accesskey="p" value="Save Settings" class="button button-primary button-large" id="publish" name="publish">
+											</div>
+											<div class="clear"></div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div id="postbox-container-2" class="postbox-container">
+							<div id="normal-sortables" class="meta-box-sortables ui-sortable">
+								<div id="<?=$options['id']?>" class="postbox  acf-postbox">
+									<button type="button" class="handlediv" aria-expanded="true"><span class="toggle-indicator" aria-hidden="true"></span></button>
+									<h2 class="hndle ui-sortable-handle"><span>Settings</span></h2>
+									<div class="inside acf-fields -top">
+										<?php
+											acf_render_fields( 'options', $fields );
+										?>
+										<script type="text/javascript">
+											if( typeof acf !== 'undefined' ) {
+												var postboxOptions = <?php echo json_encode( $options ); ?>;
+												if ( typeof acf.newPostbox === 'function' ) {
+													acf.newPostbox(postboxOptions);
+												} else if ( typeof acf.postbox.render === 'function' ) {
+													acf.postbox.render(postboxOptions);
+												}
+											}
+										</script>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br class="clear">
+					</div>
+				</div>
+			</form>
+		</div>
+		<?php
+	}
+
+	public function adminLoad() {
+		if ( acf_verify_nonce( 'options' ) ) {
+			if ( acf_validate_save_post( true ) ) {
+				acf_save_post( 'options' );
+				wp_redirect( add_query_arg( [ 'message' => '1' ] ) );
+				exit;
+			}
+		}
+		acf_enqueue_scripts();
+		wp_enqueue_script( 'post' );
 	}
 
 	/**
@@ -36,7 +122,7 @@ class Admin {
 	public function init() {
 		if ( function_exists( 'acf_add_local_field_group' ) ) {
 			acf_add_local_field_group( [
-				'key'                   => 'test_settings',
+				'key'                   => $this->key,
 				'title'                 => 'Test Settings',
 				'fields'                => [
 					// ...
